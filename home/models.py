fd486f36b0b9b07a07cc3b0e90a7076a9d5dcda2
JordanCH05 <jordi.caristiahill@gmail.com>
2022-03-24 21:43:30 +0000

    Add map model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Map(models.Model):
+    latitude = models.DecimalField(max_digits=7, decimal_places=4)
+    longitude = models.DecimalField(max_digits=7, decimal_places=4)
+    destination = models.IntegerField(default=1)
+    distance = models.DecimalField(max_digits=5, decimal_places=4)
