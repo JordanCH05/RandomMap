2252092b155ddf92cd0902d2d7224401767af513
JordanCH05 <jordi.caristiahill@gmail.com>
2022-05-04 14:41:09 +0000

    Add placeholders

--- home/forms.py
+++ home/forms.py
@@ -7,4 +7,14 @@ class MapForm(forms.ModelForm):
     
     class Meta:
         model = Map
-        fields = '__all__'
+        fields = [
+            'latitude',
+            'longitude',
+            'no_of_dests',
+            'distance',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['latitude'].widget.attrs['placeholder'] = 51.5078
+        self.fields['longitude'].widget.attrs['placeholder'] = -0.1279
